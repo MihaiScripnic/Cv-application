e74713adfa5c48a5cf12f53f88785973393cc4ac
MihaiScripnic <98556777+MihaiScripnic@users.noreply.github.com>
2022-01-27 21:01:05 +0000

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            while (true)
+            {
+                Console.Clear();
+                double firstValue, secondValue;
+                string action;
+
+                try
+                {
+                    Console.WriteLine("give the firstValue ");
+                    firstValue = double.Parse(Console.ReadLine());
+                    Console.WriteLine("give the secondValue");
+                    secondValue = double.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+
+                    Console.WriteLine("error!the program cant conveer string to number !");
+                    Console.ReadLine();
+                    continue;
+
+                }
+
+
+                Console.WriteLine("choose the operation  : '+' '-' '*' '/' ");
+
+                action = Console.ReadLine();
+
+
+
+
+                switch (action)
+                {
+                    case "+":
+                        Console.WriteLine(firstValue + secondValue);
+
+                        break;
+                    case "-":
+
+                        Console.WriteLine(firstValue - secondValue);
+                        break;
+                    case "*":
+                        Console.WriteLine(firstValue * secondValue);
+                        break;
+                    case "/":
+                        if (secondValue == 0)
+                        {
+                            Console.WriteLine(0);
+                        }
+                        else
+                        {
+                            Console.WriteLine(firstValue / secondValue);
+                        }
+
+                        break;
+                    default:
+
+                        Console.WriteLine("error! unknown operation");
+                        break;
+                }
+                Console.ReadLine();
+
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
+    
+
---
+++ Program3.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstValue, secondValue;
+            string action;
+
+            Console.WriteLine("give the firstValue ");
+            firstValue = double.Parse(Console.ReadLine());
+            Console.WriteLine("give the secondValue");
+            secondValue = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("choose the operation  : '+' '-' '*' '/' ");
+
+            action = Console.ReadLine();
+
+            if (action =="+")
+            {
+                Console.WriteLine(firstValue +  secondValue);
+            }
+            else if (action =="-")  
+            {
+                Console.WriteLine(firstValue - secondValue);
+            }else if (action =="*")
+            {
+                Console.WriteLine(firstValue * secondValue);
+
+            }else if (action =="/")
+
+            {
+                if (secondValue == 0)
+                    Console.WriteLine(0);
+                else
+                    Console.WriteLine(firstValue / secondValue);
+                } 
+                else 
+                {
+                    Console.WriteLine("error!unknown operation");
+                }
+
+                Console.ReadLine();
+            }
+
+        }
+    }
+
+    
